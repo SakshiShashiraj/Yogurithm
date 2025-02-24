b984b9b77e81ee6350154240bfd0b557fd0e62b9
SakshiShashiraj <105258978+SakshiShashiraj@users.noreply.github.com>
2025-02-23 22:12:35 -0500

    Initial Commit

---
+++ YogurithmTests/YogurithmTests.swift
@@ -0,0 +1,17 @@
+//
+//  YogurithmTests.swift
+//  YogurithmTests
+//
+//  Created by Sakshi Shashiraj on 2/23/25.
+//
+
+import Testing
+@testable import Yogurithm
+
+struct YogurithmTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
